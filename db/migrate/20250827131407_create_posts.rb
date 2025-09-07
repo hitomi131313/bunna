b6619cebe5899b04b3da67e53590b847e12f2491
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2025-09-07 11:48:19 +0000

    [Update]post

--- db/migrate/20250827131407_create_posts.rb
+++ db/migrate/20250827131407_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string  :title,          null: false, default: ""
-      t.text    :body,           null: false, default: ""
+      t.text    :body           
       t.integer :genre,          null: false, index: true, default: "0"
       t.integer :kind,           null: false, index: true, default: "0"
       t.integer :origin_country, null: false, index: true, default: "0"
