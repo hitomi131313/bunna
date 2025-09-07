a0ce0f497176f64e47c84cad28cc66e755761c28
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2025-09-07 11:51:01 +0000

    [Update]migratefile

--- db/migrate/20250827160558_add_user_id_to_post.rb
+++ db/migrate/20250827160558_add_user_id_to_post.rb
@@ -1,5 +1,5 @@
 class AddUserIdToPost < ActiveRecord::Migration[6.1]
   def change
-    add_column :posts, :user_id, :integer, null: false, default: ""
+    add_column :posts, :user_id, :integer, null: false
   end
 end
