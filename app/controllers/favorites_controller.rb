f64b799e0bb46643e256dd74dab9036a1e675947
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2025-09-28 13:30:59 +0000

    [Add]favorite非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,21 @@
 class FavoritesController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
-    favorite.save
+    @post = Post.find(params[:post_id])
+    current_user.favorites.create(post: @post)
+    respond_to do |format|
+      format.html {redirect_to @post}
+      format.js
+    end
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.find_by(post_id: post.id)
-    favorite.destroy
+    @post = Post.find(params[:post_id])
+    current_user.favorites.find_by(post: @post)&.destroy
+    respond_to do |format|
+      format.html {redirect_to @post}
+      format.js
+    end
   end
 
 end
