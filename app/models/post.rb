e385cbb6bcd8fcf87e09f0cfe674ffbd37e7cd77
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2026-01-31 13:13:00 +0000

    post.rb get_image,get_square_image コード修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,7 +15,7 @@ class Post < ApplicationRecord
   
   def get_image
     unless image.attached?
-      file_path = Rails.root.join('app/asserts/images/no_image.jpg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
     image
@@ -23,7 +23,7 @@ class Post < ApplicationRecord
 
   def get_square_image
     unless image.attached?
-      file_path = Rails.root.join('app/asserts/images/no_image.jpg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
     image.variant(gravity: :center, resize:"400x400^", crop:"400x400+0+0")
