5fc9b1815ba0cc7fc9a0c6894ae1abe7e91519a6
hitomi131313 <214909079+hitomi131313@users.noreply.github.com>
2026-02-10 12:57:20 +0000

    jpeg形式のみアップトード可能なバリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,16 @@ class Post < ApplicationRecord
   validates :origin_country,  presence: true
   validates :place,           presence: true
   
+  validate :image_content_type
+  
+  def image_content_type
+    return unless image.attached?
+
+    unless image.content_type.in?(%w(image/jpeg))
+      errors.add(:image, 'はJPEG形式のみアップロード可能です')
+    end
+  end
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
